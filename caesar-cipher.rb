8b2d213e107b3bd7d3d8117d48d6c4f007e5a4c9
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-02-23 17:29:12 +0100

    Create method to compare letters to a base array

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,3 +1,18 @@
+
+
 def caesar_cipher(string, number)
+  alphabet_array = ["a", "b", "c", "d", "e",
+                    "f", "g", "h", "i", "j",
+                    "k", "l", "m", "n", "o",
+                    "p", "q", "r", "s", "t",
+                    "u", "v", "w", "x", "y", "z"]
   # magic code
+  my_string_array = string.chars
+  p my_string_array.map {|letter| alphabet_array.index(letter)}
+  
+
+  #use index function
 end
+
+#test
+caesar_cipher("What a string!", 0)
