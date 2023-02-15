e1908eeea00c2993acb2d6df62c2264f81e9a543
Thomas Rialland <rialland.thomas@hotmail.fr>
2023-02-15 11:55:43 +0100

    Add tests for all possible cases

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,7 +1,23 @@
 require './lib/caesar_cipher.rb'
 
 describe '#caesar_cipher' do
+  it 'works with a basic string' do
+    expect(caesar_cipher('abcde', 5)).to eql('fghij')
+  end
+
+  it 'wrap from z to a' do
+    expect(caesar_cipher('vwx', 3)).to eql('yza')
+  end
+
+  it 'keeps the same case' do
+    expect(caesar_cipher('AbCdE', 5)).to eql('FgHiJ')
+  end
+
+  it 'support spaces and punctuation' do
+    expect(caesar_cipher('a! bc. d; e?', 5)).to eql('f! gh. i; j?')
+  end
+
   it 'passes the Odin example test' do
-    expect(caesar_cipher("What a string!", 5)).to eql("Bmfy f xywnsl!")
+    expect(caesar_cipher('What a string!', 5)).to eql('Bmfy f xywnsl!')
   end
 end
